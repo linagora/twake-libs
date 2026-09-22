d5282ae785af2a3b778852e28e43e4c9ebf108a8
Camille Moussu <cmoussu@linagora.com>
2026-09-22 12:28:16 +0200

    feat: Add URI template utils for chat, mail and calendar

--- packages/twake-utils/src/index.ts
+++ packages/twake-utils/src/index.ts
@@ -1,1 +1,15 @@
-export {}
+export {
+  resolveUriTemplate,
+  type UriTemplateContext
+} from './uriTemplates/uriTemplateUtils'
+export {
+  resolveMailSpaUrl,
+  buildMailComposerUrl,
+  generateMailComposerUrl
+} from './uriTemplates/mailSpaUrl'
+export { resolveChatSpaUrl } from './uriTemplates/chatSpaUrl'
+export {
+  resolveCalendarSpaUrl,
+  buildCalendarEventUrl,
+  generateCalendarEventUrl
+} from './uriTemplates/calendarSpaUrl'
---
+++ packages/twake-utils/src/uriTemplates/index.ts
@@ -0,0 +1,15 @@
+export { resolveUriTemplate, type UriTemplateContext } from './uriTemplateUtils'
+
+export {
+  resolveMailSpaUrl,
+  buildMailComposerUrl,
+  generateMailComposerUrl
+} from './mailSpaUrl'
+
+export { resolveChatSpaUrl } from './chatSpaUrl'
+
+export {
+  resolveCalendarSpaUrl,
+  buildCalendarEventUrl,
+  generateCalendarEventUrl
+} from './calendarSpaUrl'
